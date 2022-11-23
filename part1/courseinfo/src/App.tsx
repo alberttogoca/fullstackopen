763735de53e0e9cad5977202f6b550678feeba9c
alberttogoca <alberttogoca@gmail.com>
2022-11-23 19:56:33 +0100

    Fix App.tsx from last commit

--- part1/courseinfo/src/App.tsx
+++ part1/courseinfo/src/App.tsx
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
+import Content from './components/Content';
+import Header from './components/Header';
+import Total from './components/Total';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+export default function App(): JSX.Element {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
-  return <div>code here</div>;
-};
+  return (
+    <>
+      <div>
+        <Header title={course.name} />
+        <Content parts={course.parts} />
+        <Total exercises={course.parts.map((i) => i.exercises)} />
+      </div>
+    </>
+  );
+}
